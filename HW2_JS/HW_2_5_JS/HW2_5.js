b81e6e4ec8325c128742a18ffa2e7077f9c1433c
Добряк1986 <sergeyjurzanko1986@gmail.com>
2025-12-14 22:08:18 +0200

    виправлення послідовності в директоріях

---
+++ HW2_JS/HW_2_5_JS/HW2_5.js
@@ -0,0 +1,68 @@
+// описати масив, в якому буде зберігатись інформація про температуру вранці, вдень і ввечері за термін в 7 днів.
+
+let temps = [
+    {
+        day:`Monday `,
+        value:
+            {
+                morning: 10,
+                day: 20,
+                night: 12,
+            }
+    },
+    {
+        day:`Tuesday `,
+        value:
+            {
+                morning: 12,
+                day: 22,
+                night: 14,
+            }
+    },
+    {
+        day:`Wednesday `,
+        value:
+            {
+                morning: 13,
+                day: 23,
+                night: 15,
+            }
+    },
+    {
+        day:`Thursday `,
+        value:
+            {
+                morning: 12,
+                day: 25,
+                night: 15,
+            }
+    },
+    {
+        day:`Friday `,
+        value:
+            {
+                morning: 13,
+                day: 25,
+                night: 14,
+            }
+    },
+    {
+        day:`Saturday `,
+        value:
+            {
+                morning: 15,
+                day: 26,
+                night: 15,
+            }
+    },
+    {
+        day:`Sunday `,
+        value:
+            {
+                morning: 14,
+                day: 25,
+                night: 14,
+            }
+    }
+]
+console.log(temps);
