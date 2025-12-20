43e0c66558f1b1307dc14360937296bbb2866021
Добряк1986 <sergeyjurzanko1986@gmail.com>
2025-12-20 19:38:46 +0200

    додана робота 2-9

---
+++ HW2_JS/HW_2_9_JS/HW_2_9.js
@@ -0,0 +1,48 @@
+// – Скласти розклад на тиждень за домопогою switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа, що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+switch (1) {
+    case 1:
+        console.log(`plane 1`)
+        console.log(`plane 1`)
+        console.log(`plane 1`)
+        console.log(`plane 1`)
+        break;
+    case 2:
+        console.log(`plane 2`)
+        console.log(`plane 2`)
+        console.log(`plane 2`)
+        console.log(`plane 2`)
+        break;
+    case 3:
+        console.log(`plane 3`)
+        console.log(`plane 3`)
+        console.log(`plane 3`)
+        console.log(`plane 3`)
+        break;
+    case 4:
+        console.log(`plane 4`)
+        console.log(`plane 4`)
+        console.log(`plane 4`)
+        console.log(`plane 4`)
+        break;
+    case 5:
+        console.log(`plane 5`)
+        console.log(`plane 5`)
+        console.log(`plane 5`)
+        console.log(`plane 5`)
+        break;
+    case 6:
+        console.log(`plane 6`)
+        console.log(`plane 6`)
+        console.log(`plane 6`)
+        console.log(`plane 6`)
+        break;
+    case 7:
+        console.log(`plane 7`)
+        console.log(`plane 7`)
+        console.log(`plane 7`)
+        console.log(`plane 7`)
+        break;
+    default:
+        console.log(`??????`)
+}
