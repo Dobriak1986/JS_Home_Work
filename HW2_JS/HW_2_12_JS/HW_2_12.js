46b0b4e1b09e62c593fb01950d579166491661de
Добряк1986 <sergeyjurzanko1986@gmail.com>
2025-12-20 22:42:54 +0200

    додана робота 2-12 + запитання

---
+++ HW2_JS/HW_2_12_JS/HW_2_12.js
@@ -0,0 +1,36 @@
+let coursesAndDurationArray = [
+        {title: 'JavaScript Complex', monthDuration: 5},
+        {title: 'Java Complex', monthDuration: 6},
+        {title: 'Python Complex', monthDuration: 6},
+        {title: 'QA Complex', monthDuration: 4},
+        {title: 'FullStack', monthDuration: 7},
+        {title: 'Frontend', monthDuration: 4},
+    ];
+    let courses = coursesAndDurationArray [0];
+if (courses.monthDuration >= 5) {
+    console.log(`Супер`)
+}
+ courses = coursesAndDurationArray [1];
+if (courses.monthDuration >= 5) {
+    console.log(`Супер`)
+}
+courses = coursesAndDurationArray [2];
+if (courses.monthDuration >= 5) {
+    console.log(`Супер`)
+}
+courses = coursesAndDurationArray [3];
+if (courses.monthDuration >= 5) {
+    console.log(`Супер`)
+}
+courses = coursesAndDurationArray [4];
+if (courses.monthDuration >= 5) {
+    console.log(`Супер`)
+}
+courses = coursesAndDurationArray [5];
+if (courses.monthDuration >= 5) {
+    console.log(`Супер`)
+}
+
+
+
+
