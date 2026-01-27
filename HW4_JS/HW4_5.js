aa73bf085fd5c810e88c6f2f3b0e2ca13e217ece
Добряк1986 <sergeyjurzanko1986@gmail.com>
2026-01-27 17:09:44 +0200

    додана робота 4,5

---
+++ HW4_JS/HW4_5.js
@@ -0,0 +1,6 @@
+// – створити функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+
+function asdPar(text) {
+    document.write(`<p>${text}</p>`);
+}
+asdPar(`ttttt`)
