e7ebb8d5321db2f7b3849e8811ac6bfbf02a8708
Добряк1986 <sergeyjurzanko1986@gmail.com>
2026-01-27 16:41:44 +0200

    додана робота 4,3

---
+++ HW4_JS/HW4_3.js
@@ -0,0 +1,12 @@
+// – створити функцію, яка обчислює та повертає площу циліндру висотою h, та радіусом r
+// формула 2πrh
+// r = radius
+// h = height
+
+function sqCil (r, h) {
+    const res = 2 * Math.PI * r * h;
+
+    console.log(res);
+    return res;
+}
+let sqC = sqCil(2, 5);
