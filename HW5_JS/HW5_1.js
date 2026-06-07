172f644c6178ec91011f87560fbd2413721025bf
Добряк1986 <sergeyjurzanko1986@gmail.com>
2026-06-07 20:25:39 +0300

    додана робота 5.2

--- HW5_JS/HW5_1.js
+++ HW5_JS/HW5_1.js
@@ -1,9 +1,5 @@
 // #I2XsG6f
 //
 // – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами а і б
-function plos(a, b) {
-    const p = (a + b)*2;
-    console.log(p);
-    return p;
-}
-plos(5, 10);
+let prSqure = (a,b) => (a+b)*2;
+console.log(prSqure(5,10));
---
+++ HW5_JS/HW5_2.js
@@ -0,0 +1,7 @@
+// #ETGAxbEn8l
+//
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+let roundSquare = (radius) => Math.PI*radius*radius;
+console.log(roundSquare(7));
+
+
