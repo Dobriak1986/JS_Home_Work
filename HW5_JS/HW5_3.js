f4f61d0c641f08f3f7d5d298dd6eb0c50cb3f0ae
Добряк1986 <sergeyjurzanko1986@gmail.com>
2026-06-07 20:37:16 +0300

    додана робота 5.3

---
+++ HW5_JS/HW5_3.js
@@ -0,0 +1,6 @@
+// #Mbiz5K4yFe7
+//
+// – створити функцію, яка обчислює та повертає площу циліндру висотою h, та радіусом r
+// S = 2 π R h
+let squareCilin = (height, radius) => 2*Math.PI*height*radius;
+console.log(squareCilin(7,10));
